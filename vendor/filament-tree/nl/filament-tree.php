07b2e787436bbb8ce9aa788de0b268cbcfddafdc
Imac Stijn <imac_stijn@iMac-van-Imac.local>
2025-08-05 08:04:27 +0200

    Stijn_05_08_2025_EersteCommit

---
+++ vendor/filament-tree/nl/filament-tree.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'root' => 'Root',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Buttons
+        |--------------------------------------------------------------------------
+    */
+    'button.save' => 'Opslaan',
+    'button.expand_all' => 'Alles uitvouwen',
+    'button.collapse_all' => 'Alles invouwen',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Form
+        |--------------------------------------------------------------------------
+    */
+    'components.tree.buttons.select_all.label' => 'Alles selecteren',
+    'components.tree.buttons.deselect_all.label' => 'Alles deselecteren',
+    'components.tree.buttons.expand_all.label' => 'Alles uitvouwen',
+    'components.tree.buttons.collapse_all.label' => 'Alles invouwen',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Message
+        |--------------------------------------------------------------------------
+    */
+    'actions.delete.confirmation.with_children' => 'Ben je zeker dat je deze categorie en zijn kids wil wissen?',
+];
